827b245fdf21344f77fd6b2136fca656caaaa2c1
athoscannavo <104213464+athoscannavo@users.noreply.github.com>
2025-12-09 21:12:26 +0000

    Alfa Version - Super Trunfo de Estados Brasileiros
    It finally works! Future upgrades might include changing the population and PIB variables to float, for better representation.

--- super_trunfo.c
+++ super_trunfo.c
@@ -6,24 +6,24 @@ int main(){
     char estado01[50];
     char codigo01[50];
     char cidade01[50];
-    int populacao01;
-    int area01;
-    int pib01;
+    float populacao01;
+    float area01;
+    float pib01;
     int pontos_turisticos01;
 
     //Variáveis da carta 02
     char estado02[50];
     char codigo02[50];
     char cidade02[50];
-    int populacao02;
-    int area02;
-    int pib02;
+    float populacao02;
+    float area02;
+    float pib02;
     int pontos_turisticos02;
 
     /*Inicialização do programa*/
 
-    printf("Bem-vindo ao Super Trunfo de Países!!/n/n")
-    printf("Para começar, te ajudaremos a construir seu baralho./n Por favor, siga as instruções:/n/n")
+    printf("Bem-vindo ao Super Trunfo de Países!!\n\n");
+    printf("Para começar, te ajudaremos a construir seu baralho.\nPor favor, siga as instruções: \n\n");
     
 
 
@@ -33,56 +33,58 @@ int main(){
 
     //Coleta de dados carta Nº01:
     
-    printf("Carta Nº01/n")
+    printf("Carta Nº01\n");
     
-    printf("Digite uma letra de A à H para determinar o estado da carta: /n")
-    scanf ("%s", estado01)
+    printf("Digite uma letra de A à H para determinar o estado da carta: \n");
+    scanf ("%s", estado01);
 
-    printf("Digite a letra escolhida anteriormente junto de um número de 01 a 04 para determinar o código da carta(Ex: A01): /n")
-    scanf("%s", codigo01)
+    printf("Digite a letra escolhida anteriormente junto de um número de 01 a 04 para determinar o código da carta(Ex: A01): \n");
+    scanf("%s", codigo01);
 
-    printf("Digite o nome da cidade representada na carta: /n")
-    scanf("%s", cidade01)
+    printf("Digite o nome da cidade representada na carta: \n");
+    scanf("%s", cidade01);
 
-    print("Digite, em números, a população da cidade escolhida: /n")
-    scanf("%f", populacao01)
+    printf("Digite, em números, a população da cidade escolhida: \n");
+    scanf("%f", populacao01);
 
-    print("Digite, em números, a área em kilômetros quadrados da cidade escolhida: /n")
-    scanf("%f", area01)
+    printf("Digite, em números, a área em kilômetros quadrados da cidade escolhida: \n");
+    scanf("%f", area01);
 
-    print("Digite, em números, o PIB da cidade escolhida: /n")
-    scanf("%f", pib01)
+    printf("Digite, em números, o PIB da cidade escolhida: \n");
+    scanf("%f", pib01);
     
-    print("Digite, em números, quantos pontos turísticos existem na cidade: /n")
-    scanf("%d", pontos_turisticos01)
+    printf("Digite, em números, quantos pontos turísticos existem na cidade: \n");
+    scanf("%d", pontos_turisticos01);
 
 
 
     //Coleta de dados carta Nº02:
     
-    printf("Carta Nº02/n")
+    printf("Carta Nº02\n");
     
-    printf("Digite uma letra de A à H para determinar o estado da carta: /n")
-    scanf ("%s", estado02)
+    printf("Digite uma letra de A à H para determinar o estado da carta: \n");
+    scanf ("%s", estado02);
 
-    printf("Digite a letra escolhida anteriormente junto de um número de 01 a 04 para determinar o código da carta(Ex: A01): /n")
-    scanf("%s", codigo02)
+    printf("Digite a letra escolhida anteriormente junto de um número de 01 a 04 para determinar o código da carta(Ex: A01): \n");
+    scanf("%s", codigo02);
 
-    printf("Digite o nome da cidade representada na carta: /n")
-    scanf("%s", cidade02)
+    printf("Digite o nome da cidade representada na carta: \n");
+    scanf("%s", cidade02);
 
-    print("Digite, em números, a população da cidade escolhida: /n")
-    scanf("%f", populacao02)
+    printf("Digite, em números, a população da cidade escolhida: \n");
+    scanf("%f", populacao02);
 
-    print("Digite, em números, a área em km² da cidade escolhida: /n")
-    scanf("%f", area02)
+    printf("Digite, em números, a área em km² da cidade escolhida: \n");
+    scanf("%f", area02);
 
-    print("Digite, em números, o PIB em R$ da cidade escolhida: /n")
-    scanf("%f", pib02)
+    printf("Digite, em números, o PIB em R$ da cidade escolhida: \n");
+    scanf("%f", pib02);
+
+    printf("Digite, em números, quantos pontos turísticos existem na cidade: \n");
+    scanf("%d", pontos_turisticos02);
+
     
-    print("Digite, em números, quantos pontos turísticos existem na cidade: /n")
-    scanf("%d", pontos_turisticos02)
 
-
+    return 0;
 
 }
